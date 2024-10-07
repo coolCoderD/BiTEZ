1a6a79374ec56be54d3efcf0161c9beb3e7b7de2
coolCodeD <animeeditsbyme@gmail.com>
2024-10-07 23:15:17 +0530

    code

---
+++ Backend/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoutes.js";
+import cartRouter from "./routes/cartRoutes.js";
+import 'dotenv/config';
+import orderRouter from "./routes/orderRoutes.js";
+
+const app = express();
+const port = 4000;
+
+// Configure CORS
+const corsOptions = {
+    origin: '*', // Replace '*' with the frontend's domain for production (e.g., 'https://your-frontend.com')
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept','token']
+};
+
+app.use(cors(corsOptions));  // Apply CORS with options
+
+// Middleware to parse JSON request body
+app.use(express.json());
+
+// db connection
+connectDB();
+
+// API Endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'));  // For serving static files (like images)
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("api/order",orderRouter)
+
+app.get('/', (req, res) => {
+    res.send("Hello from server");
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
