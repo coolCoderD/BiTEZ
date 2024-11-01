87609b5927fba20b3046e4acbc854641bcb34b90
coolCodeD <animeeditsbyme@gmail.com>
2024-11-01 11:43:52 +0530

    backend deployed

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://bitez.onrender.com";
     const [token, setToken] = useState(localStorage.getItem("token") || "");
     const [food_list, setFoodList] = useState([]);
     const [likedItems, setLikedItems] = useState([]);
